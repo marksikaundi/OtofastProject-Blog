41e85f8d038ca955f5cba8a973669007449d5c84
marksikaundi <emmanuelmark117@gmail.com>
2023-10-04 17:17:48 +0200

    fixed header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,10 +6,13 @@ const Header = () => {
   return (
     <header className="w-full p-4 px-10 flex items-center justify-between">
       <Logo />
-      <nav className="w-max -pt-3 pl-8 border border-solid border-dark rounded-full font-medium capitalize flex items-center">
+      <nav className="w-max py-3 pl-8 border border-solid border-dark rounded-full 
+      font-medium capitalize flex items-center 
+      fixed top-6 right-1/2 translate-x-1/2">
         <Link href="/" className="mr-2">Home</Link>
-        <Link href="/about" className="mr-2">About</Link>
+        <Link href="/about" className="mx-2">About</Link>
         <Link href="/contact" className="mr-2">Contact</Link>
+        <button>T</button>
       </nav>
       <div className="">Social links</div>
     </header>
