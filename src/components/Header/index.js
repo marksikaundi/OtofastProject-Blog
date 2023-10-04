999f255f03264ae56ca5ff082b8749f3bfd6fa8a
marksikaundi <emmanuelmark117@gmail.com>
2023-10-04 17:20:02 +0200

    header done now to social links

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,7 +8,7 @@ const Header = () => {
       <Logo />
       <nav className="w-max py-3 pl-8 border border-solid border-dark rounded-full 
       font-medium capitalize flex items-center 
-      fixed top-6 right-1/2 translate-x-1/2">
+      fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm">
         <Link href="/" className="mr-2">Home</Link>
         <Link href="/about" className="mx-2">About</Link>
         <Link href="/contact" className="mr-2">Contact</Link>
