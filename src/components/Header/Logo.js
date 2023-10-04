cdde3dddf62fb1d1d4732a09ee1cd52391f29155
marksikaundi <emmanuelmark117@gmail.com>
2023-10-04 17:08:45 +0200

    header progressing

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -5,15 +5,15 @@ import profileImg from '@/public/profile-img.png'
 
 const Logo = () => {
   return (
-    <Link href="">
-        <div className='w-16 rounded-full overflow-hidden border-dark border border-solid '>
+    <Link href="/" className='flex items-center text-dark'>
+        <div className='w-16 mr-4 rounded-full overflow-hidden border-dark border border-solid '>
             <Image
             src={profileImg} 
             alt='Otofast Org'
             className='w-full h-auto rounded-full'
              />
         </div>
-        <span>OtofastOrg</span>
+        <span className='font-bold text-xl'>fastOrg</span>
     </Link>
   )
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import Logo from './Logo'
+import React from "react";
+import Link from "next/link";
+import Logo from "./Logo";
 
 const Header = () => {
   return (
-    <header className='w-full p-4 px-10 flex items-center justify-between'>
-      <Logo/>
+    <header className="w-full p-4 px-10 flex items-center justify-between">
+      <Logo />
+      <nav className="w-max -pt-3 pl-8 border border-solid border-dark rounded-full font-medium capitalize flex items-center">
+        <Link href="/" className="mr-2">Home</Link>
+        <Link href="/about" className="mr-2">About</Link>
+        <Link href="/contact" className="mr-2">Contact</Link>
+      </nav>
+      <div className="">Social links</div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
