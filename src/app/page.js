7bb36c3e5e58e153eccca06b09763ad9c78212bd
marksikaundi <emmanuelmark117@gmail.com>
2023-10-05 17:49:48 +0200

    added completed builds

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
-import Image from "next/image";
+import {allBlogs} from "contentlayer/generated";
+import HomeCoverSection from "../components/Home/HomeCoverSection";
+import FeaturedPosts from "../components/Home/FeaturedPosts";
+import RecentPosts from "../components/Home/RecentPosts";
 
 export default function Home() {
+  
   return (
     <main className="flex flex-col items-center justify-center">
-      Hello world
+      <HomeCoverSection blogs={allBlogs} />
+      <FeaturedPosts blogs={allBlogs} />
+      <RecentPosts blogs={allBlogs} />
+
+
     </main>
-  );
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
@@ -14,12 +15,29 @@ module.exports = {
         accentDark: "#ffdb4d",
         gray: "#747474",
       },
-      fontFamily: {
+      fontFamily:{
         mr: ["var(--font-mr)"],
-        in:["var(--font-in)"]
+        in: ["var(--font-in)"]
       },
+      animation:{
+        roll: "roll 24s linear infinite"
+      },
+      keyframes:{
+        roll:{
+          "0%": {transform: "translateX(100%)"},
+          "100%": {transform: "translateX(-100%)"}
+        }
+      },
+      screens:{
+        sxl: "1180px",
+        // @media (min-width: 1180px){...}
+        xs: "480px"
+        // @media (min-width: 480px){...}
+      }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
 };
-
